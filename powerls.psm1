fdefa19cd2ff686d89a926eb96f74f504cdb7d74
ralesi <scio62@gmail.com>
2016-07-18 07:27:49 -0700

    remove line endings, change file size to human readable

--- powerls.psm1
+++ powerls.psm1
@@ -1,76 +1,96 @@
 <#
- .Synopsis
+  .Synopsis
   Powershell unix-like ls
   Written by Jesse Jurman (JRJurman)
 
- .Description
+  .Description
   A colorful ls
 
- .Parameter Redirect
-  The first month to display.
+  .Parameter Lspath
+  The path to show
 
- .Example
-   # List the current directory
-   PowerLS
+  .Example
+  # List the current directory
+  PowerLS
 
- .Example
-   # List the parent directory
-   PowerLS ../
+  .Example
+  # List the parent directory
+  PowerLS ../
 #>
 function PowerLS {
   param(
-    [string]$redirect = "."
+    [Parameter(Position=0)]
+    [string]$lspath = ".",
+    [Alias('a')]
+    [switch]
+    $HiddenFiles = $null
   )
-    write-host "" # add newline at top
+  # write-host "" # add newline at top
 
-    # get the console buffersize
-    $buffer = Get-Host
-    $bufferwidth = $buffer.ui.rawui.buffersize.width
+  # get the console buffersize
+  $buffer = Get-Host
+  $bufferwidth = $buffer.ui.rawui.buffersize.width
 
-    # get all the files and folders
-    $childs = Get-ChildItem $redirect
+  # get all the files and folders
+  if ($HiddenFiles) {
+    $childs = Get-ChildItem $lspath 
+  }
+  else {
+    $childs = Get-ChildItem $lspath -Exclude ".*"
+  }
 
-    # get the longest string and get the length
-    $lnStr = $childs | select-object Name | sort-object { "$_".length } -descending | select-object -first 1
-    $len = $lnStr.name.length
+  # get the longest string and get the length
+  $lnStr = $childs | select-object Name | sort-object { "$_".length } -descending | select-object -first 1
+  $len = $lnStr.name.length
 
-    # keep track of how long our line is so far
-    $count = 0
+  # keep track of how long our line is so far
+  $count = 0
 
-    # extra space to give some breather space
-    $breather = 4
+  # extra space to give some breather space
+  $breather = 2
 
-    # for every element, print the line
-    foreach ($e in $childs) {
+  # for every element, print the line
+  foreach ($e in $childs) {
 
-      $newName = $e.name + (" "*($len - $e.name.length+$breather))
-      $count += $newName.length
+    $newName = $e.name + (" "*($len - $e.name.length+$breather))
+    $count += $newName.length
 
-      # determine color we should be printing
-      # Blue for folders, Green for files, and Gray for hidden files
-      if (Test-Path ($redirect + "\" + $e) -pathtype container) { #folders
-        write-host $newName -nonewline -foregroundcolor blue
-      }
-      elseif ($newName -match "^\..*$") { #hidden files
-        write-host $newName -nonewline -foregroundcolor darkgray
-      }
-      elseif ($newName -match "\.[^\.]*") { #normal files
-        write-host $newName -nonewline -foregroundcolor green
-      }
-      else { #others...
-        write-host $newName -nonewline -foregroundcolor white
-      }
-
-      if ( $count -ge ($bufferwidth - ($len+$breather)) ) {
-        write-host ""
-        $count = 0
-      }
-
+    # determine color we should be printing
+    # Blue for folders, Green for files, and Gray for hidden files
+    if (Test-Path ($lspath + "\" + $e) -pathtype container) { #folders
+      write-host $newName -nonewline -foregroundcolor blue
+    }
+    elseif ($newName -match "^\..*$") { #hidden files
+      write-host $newName -nonewline -foregroundcolor darkgray
+    }
+    elseif ($newName -match "\.[^\.]*") { #normal files
+      write-host $newName -nonewline -foregroundcolor green
+    }
+    else { #others...
+      write-host $newName -nonewline -foregroundcolor white
     }
 
-    write-host "" # add newline at bottom
-    write-host "" # add newline at bottom
+    if ( $count -ge ($bufferwidth - ($len+$breather)) ) {
+      write-host ""
+      $count = 0
+    }
+
+  }
+
+  write-host "" # add newline at bottom
+  # write-host "" # add newline at bottom
 
 }
 
-export-modulemember -function PowerLS
+
+function ls-short { & Get-ChildItem $args | Format-Wide -AutoSize  }
+Set-Alias -Name lss -Value ls-short -Force -Option AllScope
+
+Set-Alias -Name ll -Value gci -Force -Option AllScope
+
+Set-item alias:ls -Value 'PowerLS'
+
+# Human readable sizes in ls
+Update-FormatData -pre $PSScriptRoot/formats/Dir.Format.PS1xml
+
+export-modulemember -function PowerLS -alias ls
