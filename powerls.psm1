e904f0d335953c26397fe2df3f29aebd65418565
ralesi <scio62@gmail.com>
2016-07-19 08:21:08 -0700

    fix directory check, add hidden file flag

--- powerls.psm1
+++ powerls.psm1
@@ -25,61 +25,51 @@ function PowerLS {
     [switch]
     $HiddenFiles = $null
   )
-  # write-host "" # add newline at top
 
   # get the console buffersize
-  $buffer = Get-Host
-  $bufferwidth = $buffer.ui.rawui.buffersize.width
+    $BufferWidth = (Get-Host).ui.rawui.buffersize.width
 
   # get all the files and folders
   if ($HiddenFiles) {
-    $childs = Get-ChildItem $lspath 
+    $Childs = @(Get-ChildItem $lspath) 
   }
   else {
-    $childs = Get-ChildItem $lspath -Exclude ".*"
+    $Childs = @(Get-ChildItem $lspath -Exclude ".*")
   }
 
   # get the longest string and get the length
-  $lnStr = $childs | select-object Name | sort-object { "$_".length } -descending | select-object -first 1
-  $len = $lnStr.name.length
+    $LargestLength = ($Childs | % { $_.Name.Length } | Measure-Object -Maximum).Maximum
 
   # keep track of how long our line is so far
-  $count = 0
-
-  # extra space to give some breather space
-  $breather = 2
+    $CurrentColumn = 0
 
   # for every element, print the line
-  foreach ($e in $childs) {
-
-    $newName = $e.name + (" "*($len - $e.name.length+$breather))
-    $count += $newName.length
+    foreach ($e in $Childs) {
+      $Name = $e.Name
 
     # determine color we should be printing
     # Blue for folders, Green for files, and Gray for hidden files
-    if (Test-Path ($lspath + "\" + $e) -pathtype container) { #folders
-      write-host $newName -nonewline -foregroundcolor blue
+      if ($e.PSIsContainer) { #folders
+        write-host "$Name/  " -nonewline -foregroundcolor blue
+      } elseif ($Name -match "^\..*$") { #hidden files
+        write-host "$Name   " -nonewline -foregroundcolor darkgray
+      } elseif ($Name -match "\.[^\.]*") { #normal files
+        write-host "$Name   " -nonewline -foregroundcolor green
+      } else { #others...
+        write-host "$Name   " -nonewline -foregroundcolor white
     }
-    elseif ($newName -match "^\..*$") { #hidden files
-      write-host $newName -nonewline -foregroundcolor darkgray
-    }
-    elseif ($newName -match "\.[^\.]*") { #normal files
-      write-host $newName -nonewline -foregroundcolor green
-    }
-    else { #others...
-      write-host $newName -nonewline -foregroundcolor white
-    }
+      $CurrentColumn += $LargestLength + 3
 
-    if ( $count -ge ($bufferwidth - ($len+$breather)) ) {
+      if ( $CurrentColumn + $LargestLength + 3 -ge $BufferWidth ) {
       write-host ""
-      $count = 0
+        $CurrentColumn = 0
+      } else {
+        write-host -nonewline (" " * ($LargestLength - $Name.length))
     }
-
   }
-
+    if ($CurrentColumn -ne 0) {
   write-host "" # add newline at bottom
-  # write-host "" # add newline at bottom
-
+}
 }
 
 
