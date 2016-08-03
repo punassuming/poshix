aecf926c01f69f350095206535319bbda0ae1f98
ralesi <scio62@gmail.com>
2016-08-03 10:23:09 -0700

    Stop switch from showing multiple file sizes.

--- utils.ps1
+++ utils.ps1
@@ -4,16 +4,16 @@ Function hfs() {
   Param ($file)
   if(!$file.PSIsContainer) {
     switch($file.length) {
-      { $_ -gt 1tb }
-      { "{0:n1} T" -f ($_ / 1TB) }
-      { $_ -gt 1gb }
-      { "{0:n1} G" -f ($_ / 1GB) }
-      { $_ -gt 1mb }
-      { "{0:n1} M " -f ($_ / 1MB) }
-      { $_ -gt 1kb }
-      { "{0:n1} K " -f ($_ / 1KB) }
+      {$_ -gt 1tb}
+      { "{0:n1} T " -f ($_ / 1TB); continue }
+      {$_ -gt 1gb}
+      { "{0:n1} G " -f ($_ / 1GB); continue }
+      {$_ -gt 1mb}
+      { "{0:n1} M " -f ($_ / 1MB); continue }
+      {$_ -gt 1kb}
+      { "{0:n1} K " -f ($_ / 1KB); continue }
       default
-      { "{0:n1} B " -f $_}
+      { "{0:n1} B " -f $_ }
     }
   } elseif ($file.Attributes -band [IO.FileAttributes]::ReparsePoint) { #links
     "<SYMLINK> "
